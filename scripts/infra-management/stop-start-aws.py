ffc2f0d570b239e9104c7db359f1462053c1c13c
agapochkina01 <anastasiya.gapochkina01@yandex.ru>
2026-03-15 22:50:05 +0300

    Update stop-start-aws.py

--- scripts/infra-management/stop-start-aws.py
+++ scripts/infra-management/stop-start-aws.py
@@ -1,80 +1,27 @@
-#!/usr/bin/env python3
-"""
-Script for AWS EC2: list, stop and start instances
-"""
 import argparse
 import boto3
-import sys
+
+def manage_instances(action, instance_id, region='us-east-1'):
+    ec2 = boto3.client('ec2', region_name=region)
+    if action == 'start':
+        response = ec2.start_instances(InstanceIds=[instance_id])
+        print(f"Starting instance {instance_id}")
+    elif action == 'stop':
+        response = ec2.stop_instances(InstanceIds=[instance_id])
+        print(f"Stopping instance {instance_id}")
+    elif action == 'info':
+        response = ec2.describe_instances(InstanceIds=[instance_id])
+        instance = response['Reservations'][0]['Instances'][0]
+        print("Instance info:")
+        print(f"ID: {instance['InstanceId']}\nType: {instance['InstanceType']}\nState: {instance['State']['Name']}")
+    else:
+        print("Unknown action")
 
 
-parser = argparse.ArgumentParser(description="AWS EC2 actions")
-parser.add_argument("--action", type=str, help="start or stop instance by name")
-parser.add_argument("--list", action='store_true', help="List all instances")
-parser.add_argument("--host", type=str, help="Target instance name")
-parser.add_argument("--get-ip", action='store_true', help="Get public IP address of the host")
-
-args = parser.parse_args()
-
-ec2 = boto3.client("ec2")
-
-
-if args.list:
-    try:
-        response = ec2.describe_instances()
-        for reservation in response["Reservations"]:
-            for instance in reservation["Instances"]:
-                name_tag = next((tag["Value"] for tag in instance.get("Tags", []) if tag["Key"] == "Name"), "N/A")
-                state = instance["State"]["Name"]
-                print(f"NAME: {name_tag} STATE - {state}")
-    except Exception as e:
-        print(f"Error listing instances: {e}")
-        sys.exit(1)
-
-
-if args.host:
-    try:
-        response = ec2.describe_instances(
-            Filters=[
-                {"Name": "tag:Name", "Values": [args.host]}
-            ]
-        )
-
-        instances = [
-            i for r in response["Reservations"] for i in r["Instances"]
-        ]
-
-        if not instances:
-            print(f"Instance '{args.host}' not found")
-            sys.exit(1)
-
-        instance = instances[0]
-        instance_id = instance["InstanceId"]
-        state = instance["State"]["Name"]
-
-        if args.get_ip:
-            public_ip = instance.get("PublicIpAddress")
-            if public_ip:
-                print(public_ip)
-            else:
-                print(f"Instance '{args.host}' has no public IP")
-            sys.exit(0)
-
-        if args.action == "start":
-            if state == "running":
-                print(f"Instance '{args.host}' is already running")
-            else:
-                ec2.start_instances(InstanceIds=[instance_id])
-                print(f"Instance '{args.host}' starting...")
-
-        elif args.action == "stop":
-            if state == "stopped":
-                print(f"Instance '{args.host}' is already stopped")
-            else:
-                ec2.stop_instances(InstanceIds=[instance_id])
-                print(f"Instance '{args.host}' stopping...")
-        else:
-            print(f"Unknown action: {args.action}")
-
-    except Exception as e:
-        print(f"Error with instance '{args.host}': {e}")
-        sys.exit(1)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('action', choices=['start', 'stop', 'info'], help='Action to do')
+    parser.add_argument('instance_id', help='ID ec2 instance')
+    parser.add_argument('region', default='us-east-1', help='Region to action')
+    args = parser.parse_args()
+    manage_instances(args.action, args.instance_id, args.region)
